dd69694984a6c5b959b986b092951dad77f5fd7a
unknown <miquel.trallero@sdggroup.local>
2021-05-26 11:14:08 +0200

    create an ExpensesList component to simplify the code. Also, applied some visibility conditions

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
+import ExpensesList from './ExpensesList'
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -11,6 +12,11 @@ const Expenses = (props) => {
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });  
+
   
   return (
     <div> 
@@ -19,13 +25,7 @@ const Expenses = (props) => {
           selected={filteredYear} 
           onChangeFilter={filterChangeHandler}
         />
-        {props.items.map((expense) => (
-          <ExpenseItem 
-            key={expense.id} // Allows react to update the list in a more efficient way
-            title={expense.title} 
-            amount={expense.amount} 
-            date={expense.date}/>
-        ))};        
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
   );
