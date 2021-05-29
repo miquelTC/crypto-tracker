d28ee50826b7548858c7ccc478b200fc2a861bea
unknown <miquel.trallero@sdggroup.local>
2021-05-29 09:17:29 +0200

    fix the default value of the dropdown

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import NewCryptocurrency from './components/NewCryptocurrency/NewCryptocurrency';
 import Portfolio from './components/Portfolio/Portfolio';
+import Chart from './components/Chart/Chart';
 import cryptocurrencyPortfolio from './components/Portfolio/CryptocurrencyPortfolio';
 
 const App = () => {
